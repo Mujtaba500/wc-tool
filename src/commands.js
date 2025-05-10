9bc0ce8bfd73c7d53a16de2828c0fd8ebb4d5a61
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2025-05-10 22:58:21 +0500

    default to run commands

--- src/commands.js
+++ src/commands.js
@@ -25,11 +25,37 @@ const yargsInit = () => {
       type: "string", // We expect the filename here
       demandOption: false, // true if required
     })
+    .command(
+      "file [fileName]",
+      "read the file",
+      (yargs) => {
+        // return yargs.positional("port", {
+        //   describe: "port to bind on",
+        //   default: 5000,
+        // });
+      },
+      (argv) => {
+        file.logFileSize = true;
+        file.logNoOfLines = true;
+        file.logNoOfWords = true;
+        file.logNoOfCharacters = true;
+        file.fileName = argv.fileName;
+      }
+    )
     .middleware([
       (argv) => {
         if (argv._.length > 0) {
           console.log("Running command:", argv._[0]);
         }
+        // if (!argv.c && !argv.l && !argv.w && !argv.m) {
+        // file.logFileSize = true;
+        // file.logNoOfLines = true;
+        // file.logNoOfWords = true;
+        // file.logNoOfCharacters = true;
+        //   if (argv._.length > 0) {
+        //     file.fileName = argv.fileName;
+        //   }
+        // }
         console.log("argv", argv);
         if (argv.c) {
           file.fileName = argv.c;
